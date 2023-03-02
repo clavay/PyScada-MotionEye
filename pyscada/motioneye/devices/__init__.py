5640504a698399e31d1b68c05ec9635a9be60a12
clavay <clavayssiere@univ-pau.fr>
2023-03-02 17:21:55 +0100

    fixes

--- pyscada/motioneye/devices/__init__.py
+++ pyscada/motioneye/devices/__init__.py
@@ -4,6 +4,7 @@
 from __future__ import unicode_literals
 from .. import PROTOCOL_ID
 from pyscada.models import DeviceProtocol, VariableProperty
+from pyscada.device import GenericHandlerDevice
 
 import traceback
 import re
@@ -12,6 +13,15 @@ import json
 from typing import Any
 
 from asgiref.sync import async_to_sync
+from async_timeout import timeout
+from asyncio import wait_for
+try:
+    from asyncio.exceptions import TimeoutError as asyncioTimeoutError
+    from asyncio.exceptions import CancelledError as asyncioCancelledError
+except ModuleNotFoundError:
+    # for python version < 3.8
+    from asyncio import TimeoutError as asyncioTimeoutError
+    from asyncio import CancelledError as asyncioCancelledError
 
 import logging
 
@@ -25,58 +35,6 @@ except ImportError:
     driver_ok = False
 
 
-async def async_query_motioneye_server(device) -> dict[str, Any] or None:
-    try:
-        url = device.motioneye_server.url
-        admin_username = device.motioneye_server.admin_username
-        admin_password = device.motioneye_server.admin_password
-        surveillance_username = device.motioneye_server.surveillance_username
-        surveillance_password = device.motioneye_server.surveillance_password
-        async with MotionEyeClient(url, admin_username, admin_password, surveillance_username, surveillance_password) as client:
-            if not client:
-                return None
-
-            await client.async_client_login()
-            manifest = await client.async_get_manifest()
-            server_config = await client.async_get_server_config()
-            cameras = await client.async_get_cameras()
-
-            resp = dict()
-            resp['manifest'] = manifest
-            resp['server_config'] = server_config
-            resp['cameras'] = cameras
-
-            #logger.debug(json.dumps(resp, indent=5))
-            return resp
-    except MotionEyeClientInvalidAuthError:
-        logger.warning("Invalid motionEye authentication for {}.".format(device))
-    except MotionEyeClientConnectionError:
-        logger.warning("MotionEye connection failure for {}.".format(device))
-    except MotionEyeClientRequestError:
-        logger.warning("MotionEye request failure for {}.".format(device))
-    except MotionEyeClientURLParseError:
-        logger.warning("Unable to parse the URL for {}.".format(device))
-    except MotionEyeClientPathError:
-        logger.warning("Invalid path provided for {}.".format(device))
-    return None
-
-
-async def async_get_cameras_config(device):
-    # Refresh cameras configurations
-    inst = None
-    camera_config = None
-    try:
-        inst = await async_query_motioneye_server(device.motioneyedevice)
-        if type(inst) == dict and 'cameras' in inst and type(inst['cameras']) == dict and 'cameras' in inst['cameras']:
-            for camera in inst['cameras']['cameras']:
-                if camera['id'] == device.motioneyedevice.camera_id:
-                    camera_config = camera
-    except Exception as e:
-        logger.warning(e)
-        logger.warning(traceback.format_exc())
-    return inst, camera_config
-
-
 async def async_set_cameras_config(device, conf):
     try:
         url = device.motioneye_server.url
@@ -85,7 +43,10 @@ async def async_set_cameras_config(device, conf):
         surveillance_username = device.motioneye_server.surveillance_username
         surveillance_password = device.motioneye_server.surveillance_password
         async with MotionEyeClient(url, admin_username, admin_password, surveillance_username, surveillance_password) as client:
-            await client.async_set_camera(device.camera_id, conf)
+            try:
+                await wait_for(client.async_set_camera(device.camera_id, conf), timeout=5)
+            except (TimeoutError, asyncioTimeoutError):
+                logger.warning("Timeout for {}.".format(device))
     except MotionEyeClientInvalidAuthError:
         logger.warning("Invalid motionEye authentication for {}.".format(device))
     except MotionEyeClientConnectionError:
@@ -107,8 +68,10 @@ async def async_do_action(device, camera_id, action):
         surveillance_username = device.motioneye_server.surveillance_username
         surveillance_password = device.motioneye_server.surveillance_password
         async with MotionEyeClient(url, admin_username, admin_password, surveillance_username, surveillance_password) as client:
-            await client.async_action(camera_id, action)
-
+            try:
+                await wait_for(client.async_action(camera_id, action), timeout=5)
+            except (TimeoutError, asyncioTimeoutError):
+                logger.warning("Timeout for {}.".format(device))
         # Set the action variable to False to be settable again
         return False
     except MotionEyeClientInvalidAuthError:
@@ -124,57 +87,24 @@ async def async_do_action(device, camera_id, action):
     return None
 
 
-class GenericDevice:
+class GenericDevice(GenericHandlerDevice):
     def __init__(self, pyscada_device, variables):
-        self._device = pyscada_device
-        self._variables = variables
-        self.inst = None
+        super().__init__(pyscada_device, variables)
+        self._protocol = PROTOCOL_ID
+        self.driver_ok = driver_ok
         self.camera_config = None
         self.last_value = None
-        self._device_not_accessible = 0
 
     def connect(self):
         """
         establish a connection to the Instrument
         """
-        if not driver_ok:
-            return False
+        super().connect()
 
-        if self._device.protocol.id != PROTOCOL_ID:
-            logger.error("Wrong handler selected : it's for %s device while device protocol is %s" %
-                         (str(DeviceProtocol.objects.get(id=PROTOCOL_ID)).upper(),
-                          str(self._device.protocol).upper()))
-            return False
+        async_to_sync(self.async_get_cameras_config)()
 
-        self.inst, self.camera_config = async_to_sync(async_get_cameras_config)(self._device)
-
-        if self.inst is not None:
-            if self._device_not_accessible < 1:
-                self._device_not_accessible = 1
-                logger.info('Connected to MotionEye device : {}'.format(self._device))
-        else:
-            if self._device_not_accessible > -1:
-                self._device_not_accessible = -1
-                logger.info('MotionEye device {} is not accessible'.format(self._device))
+        self.accessibility()
         return True
-
-    def disconnect(self):
-        if self.inst is not None:
-            self.inst = None
-            return True
-        return False
-
-    def before_read(self):
-        """
-        will be called before the first read_data
-        """
-        return None
-
-    def after_read(self):
-        """
-        will be called after the last read_data
-        """
-        return None
 
     def read_data(self, variable_instance):
         """
@@ -184,24 +114,6 @@ class GenericDevice:
         if self.camera_config is not None and variable_instance.motioneyevariable.service in self.camera_config:
             value = self.camera_config[variable_instance.motioneyevariable.service]
         return value
-
-    def read_data_and_time(self, variable_instance):
-        """
-        read values and timestamps from the device
-        """
-
-        return self.read_data(variable_instance), self.time()
-
-    def read_data_all(self, variables_dict):
-        output = []
-
-        if self.connect():
-            for item in variables_dict.values():
-                value, read_time = self.read_data_and_time(item)
-
-                if value is not None and item.update_value(value, read_time):
-                    output.append(item.create_recorded_data_element())
-        return output
 
     def write_data(self, variable_id, value, task):
         """
@@ -249,5 +161,66 @@ class GenericDevice:
             logger.warning('Variable {} not in variable list {} of device {}'.format(variable_id, self._variables, self._device))
         return None
 
-    def time(self):
-        return time()
+    async def async_get_cameras_config(self):
+        # Refresh cameras configurations
+        self.inst = None
+        self.camera_config = None
+        try:
+            self.inst = await self.async_query_motioneye_server()
+            if type(self.inst) == dict and 'cameras' in self.inst and type(self.inst['cameras']) == dict and 'cameras' in self.inst['cameras']:
+                for camera in self.inst['cameras']['cameras']:
+                    if camera['id'] == self._device.motioneyedevice.camera_id:
+                        self.camera_config = camera
+        except (TimeoutError, asyncioTimeoutError):
+            self._not_accessible_reason = "Timeout"
+        except Exception as e:
+            self._not_accessible_reason = e
+            # logger.warning(traceback.format_exc())
+
+    async def async_query_motioneye_server(self) -> dict[str, Any] or None:
+        try:
+            url = self._device.motioneyedevice.motioneye_server.url
+            admin_username = self._device.motioneyedevice.motioneye_server.admin_username
+            admin_password = self._device.motioneyedevice.motioneye_server.admin_password
+            surveillance_username = self._device.motioneyedevice.motioneye_server.surveillance_username
+            surveillance_password = self._device.motioneyedevice.motioneye_server.surveillance_password
+            async with timeout(20) as tm:
+                client = MotionEyeClient(url,
+                                         admin_username,
+                                         admin_password,
+                                         surveillance_username,
+                                         surveillance_password)
+                try:
+                    if not client:
+                        self._not_accessible_reason = f"MotionEyeClient is {client}"
+                        return None
+                    resp = dict()
+
+                    await client.async_client_login()
+
+                    manifest = await client.async_get_manifest()
+                    server_config = await client.async_get_server_config()
+                    cameras = await client.async_get_cameras()
+
+                    resp['manifest'] = manifest
+                    resp['server_config'] = server_config
+                    resp['cameras'] = cameras
+
+                    return resp
+
+                except asyncioCancelledError:
+                    self._not_accessible_reason = "Timeout"
+                finally:
+                    await client.async_client_close()
+
+        except MotionEyeClientInvalidAuthError:
+            self._not_accessible_reason = f"Invalid motionEye authentication for {self._device.motioneyedevice}."
+        except MotionEyeClientConnectionError:
+            self._not_accessible_reason = f"MotionEye connection failure for {self._device.motioneyedevice}."
+        except MotionEyeClientRequestError:
+            self._not_accessible_reason = f"MotionEye request failure for {self._device.motioneyedevice}."
+        except MotionEyeClientURLParseError:
+            self._not_accessible_reason = f"Unable to parse the URL for {self._device.motioneyedevice}."
+        except MotionEyeClientPathError:
+            self._not_accessible_reason = f"Invalid path provided for {self._device.motioneyedevice}."
+        return None
